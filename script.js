5f1cd4d38b137e3564cc2035ce5d94b093689467
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2022-12-13 21:14:08 -0300

    checando outras tentativas

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var delayInMilliseconds = 1000;
+var delayInMilliseconds = 500;
 var randomNumb = parseInt(Math.random() * 11);
 var main = document.querySelector("main");
+var index = 0;
 
 setTimeout(function delay() {
+    console.log(randomNumb);
     main.innerHTML = `
     <section>
         <h1>Digite um número entre 0 e 10</h1>
 
         <div class="inputs">
             <input class="wrong-attempts" id="circle-3" type="number"disabled="">
-            <input class="wrong-attempts" id="circle-2"type="number" disabled="">
-            <input class="wrong-attempts" id="circle-1"type="number" disabled="">
+            <input class="wrong-attempts" id="circle-2" type="number" disabled="">
+            <input class="wrong-attempts" id="circle-1" type="number" disabled="">
             <input class="userAttempt" type="number">
         </div>
         
@@ -34,7 +36,9 @@ setTimeout(function delay() {
 }, delayInMilliseconds)
 
 function check() {
-    const userAttempt = document.querySelector(".userAttempt").value;
+    index++;
+    console.log(index);
+    var userAttempt = document.querySelector(".userAttempt").value;
 
     if (userAttempt == randomNumb) {
         console.log(`ACERTOU userNumb: ${userAttempt} randomNumb: ${randomNumb}`)
@@ -54,8 +58,78 @@ function check() {
     } else {
         console.log(`ERROU userNumb: ${userAttempt} randomNumb: ${randomNumb}`)
 
-        var circle1 = document.querySelector("#circle-1");
-        circle1.style.backgroundColor = "red";
-        
+        switch (index) {
+            case 1:
+                check1();
+                break;
+
+            case 2:
+                check2();
+                break;
+            
+            case 3:
+                check3();
+                break;
+        }
     }
+}
+
+function check1() {
+    var circle0 = document.querySelector(".userAttempt");
+    circle0.style.backgroundColor = "#4285F4";
+
+    var button = document.querySelector("button");
+    button.style.backgroundColor = "#4285F4";
+
+    var circle1 = document.querySelector("#circle-1");
+    circle1.style.backgroundColor = "#34A852";
+
+    var attempts = document.querySelector(".attempts").innerHTML = `
+        <img src="/images/wrong-icon.svg" alt="tentativa errada">
+        <img src="/images/open-icon.svg" alt="tentativa fechada">
+        <img src="/images/closet-icon.svg" alt="tentativa fechada">
+        <img src="/images/closet-icon.svg" alt="tentativa fechada">
+        `
+}
+
+function check2() {
+    var circle0 = document.querySelector(".userAttempt");
+    circle0.style.backgroundColor = "#FABB05";
+
+    var button = document.querySelector("button");
+    button.style.backgroundColor = "#FABB05";
+
+    var circle1 = document.querySelector("#circle-1");
+    circle1.style.backgroundColor = "#4285F4";
+    var circle2 = document.querySelector("#circle-2");
+    circle2.style.backgroundColor = "#34A852";
+
+    var attempts = document.querySelector(".attempts").innerHTML = `
+        <img src="/images/wrong-icon.svg" alt="tentativa errada">
+        <img src="/images/wrong-icon.svg" alt="tentativa errada">
+        <img src="/images/open-icon.svg" alt="tentativa aberta">
+        <img src="/images/closet-icon.svg" alt="tentativa fechada">
+        `
+}
+
+function check3() {
+    var circle0 = document.querySelector(".userAttempt");
+    circle0.style.backgroundColor = "#EA4335";
+
+    var button = document.querySelector("button");
+    button.style.backgroundColor = "#EA4335";
+
+    var circle1 = document.querySelector("#circle-1");
+    circle1.style.backgroundColor = "#FABB05";
+    var circle2 = document.querySelector("#circle-2");
+    circle2.style.backgroundColor = "#4285F4";
+    var circle3 = document.querySelector("#circle-3");
+    circle3.style.backgroundColor = "#34A852";
+
+    var attempts = document.querySelector(".attempts").innerHTML = `
+        <img src="/images/wrong-icon.svg" alt="tentativa errada">
+        <img src="/images/wrong-icon.svg" alt="tentativa errada">
+        <img src="/images/wrong-icon.svg" alt="tentativa errada">
+        <img src="/images/open-icon.svg" alt="tentativa aberta">
+        `
 }
